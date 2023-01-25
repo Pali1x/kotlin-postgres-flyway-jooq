7c5cdaa0019e8e4b640bd755a4beaab54e7786be
Pali1x <pavol.cina@sap.com>
2023-01-25 20:33:54 +0100

    adding jooq dependencies and configuration, generation of DSL API content

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+//(jooq) 1. add import for jooq configuration Logging
+import org.jooq.meta.jaxb.Logging
 
 // (refactor) 1. move group and version to top
 group = "org.example"
@@ -11,12 +13,16 @@ val dbPassword = "admin_password"
 val dbSchema = "my"
 
 val postgresVersion = "42.5.1"
+//(jooq) 2. initialize constant for jooq version
+val jooqVersion = "3.15.3"
 
 plugins {
     kotlin("jvm") version "1.8.0"
     application
     // (flyway) 3. add flyway plugin to be able run gradle flyway tasks main to remember: gradle flywayMigrate; gradle flywayClean
     id("org.flywaydb.flyway") version "9.11.0"
+    //(jooq) 3. add jooq plugin to be able to run database content generation, generating of table entities, types, DSL api data manipulation
+    id("nu.studer.jooq") version "6.0.1"
 }
 
 repositories {
@@ -27,6 +33,10 @@ dependencies {
     //(flyway) 4. add flyway and postgres dependencies to be able to configure and operate with them in project
     implementation("org.flywaydb:flyway-core:9.11.0")
     implementation("org.postgresql:postgresql:$postgresVersion")
+    //(jooq) 4. add jooq generator dependency on postgres database and jooq API
+    jooqGenerator("org.postgresql:postgresql:$postgresVersion")
+    api("org.jooq:jooq:$jooqVersion")
+
     testImplementation(kotlin("test"))
 }
 
@@ -64,4 +74,46 @@ flyway {
     // Whether to automatically call baseline when migrate is executed against a non-empty schema with no metadata table. (default = false)
     // Be careful when enabling this as it removes the safety net that ensures Flyway does not migrate the wrong database in case of a configuration mistake!
     baselineOnMigrate = true
+}
+
+//(jooq) 5. add jooq configuration, as JDBC configuration from which database generate conetent, where to store it
+// jooqGenerate runs before project compilation, but could be run manually by gradle generateJooq
+jooq {
+    version.set(jooqVersion)
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            generateSchemaSourceOnCompilation.set(true)
+
+            jooqConfiguration.apply {
+                logging = Logging.INFO
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = dbUrl
+                    user = dbUser
+                    password = dbPassword
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = dbSchema
+                        excludes = "key_vault|flyway_schema_history"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isRelations = true
+                        isPojos = true
+                        isPojosEqualsAndHashCode = true
+                    }
+                    target.apply {
+                        packageName = "jooq.generated"
+                    }
+                }
+            }
+        }
+    }
 }
