56726c3ae1bf88653b5a8548a91ce83013b8c000
Pali1x <pavol.cina@sap.com>
2023-01-26 15:03:42 +0100

    Database Connection implementation DSLContext

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import configuration.DatasourceConfiguration
+import org.jooq.DSLContext
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//5 define variable which will be used to access to / manipulation with database
+lateinit var dslContext: DSLContext
+
+//6 we removed generated code from main method and initialize our global dslContext variable with DslContext created by our DatasourceConfiguration class
+fun main(args: Array<String>) {
+    dslContext = DatasourceConfiguration.createDSLContext()
+
 }
